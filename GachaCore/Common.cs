59e93f11a0f6ccab4db5e79f4c6f4ff721fe07fb
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-02-11 22:54:57 +0800

    日志方法

--- GachaCore/Common.cs
+++ GachaCore/Common.cs
@@ -1,7 +1,4 @@
-﻿using GachaCore.Model;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 
 namespace GachaCore
 {
@@ -10,15 +7,29 @@ namespace GachaCore
         public static string DBPath { get; set; } = "data.db";
         public static DateTime SignRefreshTime { get; set; }
         public static int MinSignMoney { get; set; }
+        public static Action<string, string, bool> InfoMethod { get; set; } = null;
         public static int MaxSignMoney { get; set; }
         public static int RegistryMoney { get; set; }
         public static Random Random { get; set; } = new Random();
+      
         public static void Init()
         {
             SignRefreshTime = ConfigHelper.GetConfig("SignRefreshTime", new DateTime(1970, 1, 1, 4, 0, 0));
             MinSignMoney = ConfigHelper.GetConfig("MinSignMoney", 0);
             MaxSignMoney = ConfigHelper.GetConfig("MaxSignMoney", 1000);
             RegistryMoney = ConfigHelper.GetConfig("RegistryMoney", 5000);
+        }
+      
+        public static void Info(string type, string message, bool status = true)
+        {
+            if (InfoMethod == null)
+            {
+                Console.WriteLine($"{(status ? "[+]" : "[-]")}[{DateTime.Now:G}][{type}]{message}");
+            }
+            else
+            {
+                InfoMethod.Invoke(type, message, status);
+            }
         }
     }
 }
